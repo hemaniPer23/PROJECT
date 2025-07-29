abf0f9f7d44d799908f2a800f19c7fc538daf8d0
Sumanaweera12090 <sumanaweera12090@usci.ruh.ac.lk>
2025-07-29 06:13:21 +0530

    Add get_parties.php to fetch all political parties from DB

--- frontend/src/API.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-//this url was php flie location url in xampp or wamp 
-const API = axios.create({
-  baseURL: 'http://localhost/PROJECT/backend',
-});
-
-export default API;
